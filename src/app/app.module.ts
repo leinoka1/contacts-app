7bc13c5f2d71ae6a7bc0ac69d548c09604371765
leinoka1 <kari.v.leino@gmail.com>
2017-11-01 11:01:14 +0200

    Contact services added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
 import { ContactComponent } from './contact/contact.component';
 import { ContactListComponent } from './contact/contact-list/contact-list.component';
 import { ContactListItemComponent } from './contact/contact-list/contact-list-item/contact-list-item.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatIconModule, MatInputModule, MatListModule} from '@angular/material';
+import {ContactService} from './contact/services/contact.service';
 
 @NgModule({
   declarations: [
@@ -14,9 +18,16 @@ import { ContactListItemComponent } from './contact/contact-list/contact-list-it
     ContactListItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
   ],
-  providers: [],
+  providers: [ContactService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
